06dcf1659a701ae090a83cbc4aa94a82a0e3f1af
Makoto <makoto.sato.m@soka.ac.jpgmail.com>
2014-07-12 18:11:14 +0900

    NotePCcase02add

--- MeCab/SimpleCode01.py
+++ MeCab/SimpleCode01.py
@@ -3,17 +3,26 @@ import sys
 import codecs
 import MeCab
 
-tagger=MeCab.Tagger("-Ochasen")
-text=codecs.open('case01.txt','r','utf-8').read()
-encode_text = text.encode('utf-8')
-node=tagger.parse(encode_text)
-result = node.decode('utf-8')
+def extractKeyword(text):
+    """textを形態素解析して、名詞のみのリストを返す"""
+    tagger=MeCab.Tagger('-u C:\pg\MeCab\dic\ipadic\user.dic')
+    node = tagger.parseToNode(text.encode('utf-8'))
+    keywords = []
+    while node:
+        keywords.append(node.surface.decode("utf-8"))
+        node = node.next
+    return keywords
 
-for item in result:
-    print item[0],
+text=codecs.open('case02.txt','r','utf-8').read()
+nodes= extractKeyword(text)
+
+for item in nodes:
+    print item+"_",
 """
 while result:
     item = result[0]
     print item,
     if result.next: break
 """
+
+
