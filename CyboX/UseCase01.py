c180faca8d8de82f3a022bdf0422615d93251194
Hick <makoto.sato.m@gmail.com>
2014-07-12 17:50:01 +0900

    usecases

---
+++ CyboX/UseCase01.py
@@ -0,0 +1,18 @@
+import sys
+import cybox.bindings.cybox_core as cybox_core_binding
+from cyox.core import Observables
+import cybox.utils
+from cybox.objcets.win_file_object import winfile
+from cybox.objcets import
+from cybox.objcets. import
+from cybox.objcets. import
+
+def main():
+    NS = cybox.utils.Namespace("http://example.com/","example")
+    cybox.utils.set_id_namespace(NS)
+
+    m = 
+
+
+if __name__=='__main__':
+    main()
---
+++ MeCab/SimpleCode01.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+import sys
+import codecs
+import MeCab
+
+tagger=MeCab.Tagger("-u user.dic")
+text=codecs.open('case01.txt','r','utf-8').read()
+encode_text = text.encode('utf-8')
+node=tagger.parse(encode_text)
+result = node.decode('utf-8')
+
+for item in result:
+    print item[0],
+"""
+while result:
+    item = result[0]
+    print item,
+    if result.next: break
+"""
