d6b6f41bc108072d423b5cbd1e854a95a031ef66
Hick <makoto.sato.m@gmail.com>
2014-07-22 18:15:58 +0900

    名詞

--- MeCab/analyze.py
+++ MeCab/analyze.py
@@ -1,23 +1,28 @@
-#config:utf-8
+# -*- coding: utf-8 -*-
 import sys
 import codecs
 import MeCab
 
 def ExtractNorn(text):
     tagger = MeCab.Tagger(r'-u C:\Software\MeCab\dic\naistdic\user.dic')
-    node = tagger.parseToNode(text)
+    node = tagger.parseToNode(text.encode('utf-8'))
     keywords = []
+    i=10
     while node:
-        keywords.append(node.surface.decode('utf-8'))
+        item = node.feature.split(",")
+        if item[0] == r"名詞" or item[0] == r"動詞":
+            if i>0:  print node.surface
+            i -= 1
+            keywords.append(node.surface.decode('utf-8'))
         node = node.next
     return keywords
 
-text = open("Protection_of_Personal_Information.txt","r").read()
-#nodes = ExtractNorn(text)
-tagger = MeCab.Tagger(r'-u C:\Software\MeCab\dic\naistdic\user.dic')
-node = tagger.parse(text)
-txt = open("mecab.txt","w")
-txt.write(node)
+text = open("Protection_of_Personal_Information_utf8.txt","r").read()
+nodes = ExtractNorn(text)
+#tagger = MeCab.Tagger(r'-u C:\Software\MeCab\dic\naistdic\user.dic')
+#node = tagger.parse(text)
+txt = open("split.txt","w")
+txt.write(str(nodes))
 
 """
 for item in nodes:
