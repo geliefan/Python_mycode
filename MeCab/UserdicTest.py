9a7534f61de878e0ed656843ab3964dcd86b4a08
Makoto <makoto.sato.m@soka.ac.jpgmail.com>
2014-08-22 17:26:50 +0900

    なぜか同期に失敗したファイル

--- MeCab/UserdicTest.py
+++ MeCab/UserdicTest.py
@@ -3,12 +3,12 @@ import MeCab
 
 def extractKeyword(text):
     """textを形態素解析して、名詞のみのリストを返す"""
-    tagger = MeCab.Tagger('-u user.dic')
+    tagger = MeCab.Tagger(r'-u C:\pg\MeCab\dic\ntk_dic\user.dic')
     node = tagger.parseToNode(text.encode('utf-8'))
     keywords = []
     while node:
-        if node.feature.split(",")[0] == u"名詞":
-            keywords.append(node.surface.decode(utf-8))
+        if node.feature.split(",")[0] == r"名詞":
+            keywords.append(node.surface.decode('utf-8'))
         node = node.next
     return keywords
 
@@ -17,6 +17,7 @@ if __name__ == "__main__":
         あるいはそのための一連の基礎技術をさす。
         人工知能という名前は1956年にダートマス会議でジョン・マッカーシーにより命名された。
         現在では、機械学習、自然言語処理、パターン認識などの研究分野がある。"""
-    keywords = extractKeyword(str(text))
+    keywords = extractKeyword(text)
     for w in keywords:
         print w,
+
